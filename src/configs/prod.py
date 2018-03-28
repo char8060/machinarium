ed01d4e32b3f173b36583f104e5c6d61d832c003
Oleh Shyshkin <oshyshkin@gogoair.com>
2018-03-28 17:09:35 +0300

    Add PROD credentials.

--- src/configs/prod.py
+++ src/configs/prod.py
@@ -2,8 +2,8 @@
 metalayer_config = {
     'host': 'db.machinarium.prod.gogoair.com',
     'database': 'p2',
-    'user': 'machinarium',
-    'password': '',
+    'user': 'p2_rw',
+    'password': 'p#aZp89',
     'port': 3306,
     'connect_timeout': 10
 }
