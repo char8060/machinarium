10dbf9a357060cdfc3d7073e8923dd3880ce646d
Oksana Shevchuk <c.oshevchuk@gogoair.com>
2018-10-25 11:29:49 +0300

    add change for kandu_files minute

--- src/configs/schema_metadata.py
+++ src/configs/schema_metadata.py
@@ -142,7 +142,7 @@ PARTITIONS = {
      'satcom.antenna_regression_features': ["partition_date"],
      'satcom.antenna_regression_output': ["partition_date"],
      'satcom.kandu_files': ["partition_date"],
-     'satcom.kandu_files_minute_p2': ["partition_date"],
+     'satcom.kandu_files_minute': ["partition_date"],
 
      'rdp.fact_flight_availability': ["partition_date", "flight_source"],
      'rdp.fact_flight_segment': ["partition_date", "flight_source"],
